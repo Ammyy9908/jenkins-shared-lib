2668b8ba73c0d5fbf9b28c72b179e32c2454dcc2
ammyy9908 <user@Users-MacBook-Pro-2.local>
2024-11-24 12:49:55 +0530

    Push added

---
+++ vars/push.groovy
@@ -0,0 +1,9 @@
+def call(String Project,String ImageTag,String dockerhubUser){
+                 withCredentials([usernamePassword('credentialsId':"DockerHubCred",
+                passwordVariable:"dockerhubpass",
+                usernameVariable:"dockerhubUser")]){
+                     sh "docker login -u ${dockerhubUser} -p ${dockerhubpass}"
+                     
+                }
+                sh "docker push ${dockerhubUser}/${Project}:${ImageTag}"
+}
